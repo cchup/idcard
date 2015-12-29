f86bd3351c564495de99c72567af1e86b3c96d13
lixiaopeng <lixiaopeng_qt@he.chinamobile.com>
2015-12-29 14:38:22 +0800

    add spring-boot

---
+++ idcard/src/main/java/com/cchup/idcard/App.java
@@ -0,0 +1,13 @@
+package com.cchup.idcard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
